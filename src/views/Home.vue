f0ad184ed31dff97d8084835c3a927675dbf194a
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-24 20:48:07 +0200

    Render Home view on load

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <Sidebar />
+  <Main :feedbacks="feedbacks" />
+</template>
+
+<script lang="ts">
+import Sidebar from "../components/Sidebar.vue";
+import { fetchFeedbacks } from "../api/FeedbacksApi";
+import { FeedbackType } from "../types/types";
+import Main from "../components/Main.vue";
+
+export default {
+  components: {
+    Sidebar,
+    Main,
+  },
+  data() {
+    return {
+      feedbacks: [] as FeedbackType[],
+    };
+  },
+  async created() {
+    this.feedbacks = await fetchFeedbacks();
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../scss/variables";
+
+:root {
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: $primary-color;
+    font-family: $main-font;
+  }
+  #app {
+    max-width: 1280px;
+    display: flex;
+    margin: 5rem auto;
+    gap: 2rem;
+  }
+}
+</style>
