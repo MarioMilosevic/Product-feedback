63c3b3a03edb612b81af4e025b503fc5545828fd
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-25 10:22:15 +0200

    Add FeedbackDetails view route

---
+++ src/views/FeedbackDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>The feedback id is {{ id }}</p>
+</template>
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+      return {
+        id: this.$route.params.id
+    };
+  },
+  computed: {
+    // 
+  },
+  mounted() {
+    // 
+  },
+  methods: {
+    // 
+  },
+};
+</script>
