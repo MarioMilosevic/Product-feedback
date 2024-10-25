d7d9cdc0f7fb141ec2b8f4a8dc826d0742bfe197
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-10-25 18:10:06 +0200

    Render current feedback

--- src/views/FeedbackDetails.vue
+++ src/views/FeedbackDetails.vue
@@ -1,14 +1,19 @@
 <template>
-  <p>The feedback id is {{ id }}</p>
+  <div class="wrapper">
+    <Feedback :feedback="currentFeedback" />
+  </div>
 </template>
 <script lang="ts">
+import Feedback from "../components/Feedback.vue";
 import { useFeedbackStore } from "../stores/FeedbackStore";
 export default {
   setup() {
     const feedbackStore = useFeedbackStore();
     return { feedbackStore };
   },
-  components: {},
+  components: {
+    Feedback,
+  },
   props: {
     id: { type: String, required: true },
   },
@@ -20,9 +25,20 @@ export default {
       const feedbackId = Number(this.id);
       return this.feedbackStore.getFeedback(feedbackId);
     },
+    //
+  },
+  mounted() {
+    console.log(this.currentFeedback);
   },
   methods: {
     //
   },
 };
 </script>
+
+<style lang="scss">
+.wrapper {
+  border: 1px solid black;
+  width: 100%;
+}
+</style>
